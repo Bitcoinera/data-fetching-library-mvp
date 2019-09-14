ff4d7d22cdd76378e0b96dd91f44580a38927284
anagutjor <anagutjor@gmail.com>
2019-09-14 14:00:08 +0200

    add apps-artifacts. app.abi issue

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # Data fetching library MVP
 
-The main `index.js` at the first layer is run, calling functions from the imported `Aragon` class from the `wrapper/index.js` file.
+The main `index.js` is run, calling functions from the imported `Aragon` class from the `wrapper/index.js` file.
 
-Currently calling `pastEvents()`, `runApp()` and `getApp()`.
+Currently calling `pastEvents()`, `getApp()` and `runApp()`.
 
+### Goal
+
+First Goal: be able to subscribe to all the events occurring in a specific DAO. 
 
 ### Notes
 
-For it to work you need to copy the `artifacts` folder inside `wrapper` into the `dist` folder. Like this:
+For it to work you need to copy the `artifacts` folder inside the `wrapper` into the `dist` folder. Like this:
 
 ```
-cp -r wrapper/artifacts/ dist/
+cp -r node_modules/@aragon/wrapper/artifacts/ dist/
 ```
+And `apps-artifacts` too:
+
+```
+cp -r app-artifacts dist/
+```
+
+Also, I am copy-pasting the `wrapper-index.js` file into `node_modules/@aragon/wrapper/src/index.js`.
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import Aragon from './src/index';
 
 const dappwallDAO = new Aragon('0xeeda0bD313b8042B360Ac6755e01D7970860c445',
-    {apm: {ensRegistryAddress: '0x98df287b6c145399aaa709692c8d308357bc085d'}},
-    {events: {subscriptionDelayTime: 180000} // 3 mins
-}) // rinkeby ensRegistry
+    {apm: {ensRegistryAddress: '0x98df287b6c145399aaa709692c8d308357bc085d'}}
+) // rinkeby ensRegistry
 
 // '0xeeda0bD313b8042B360Ac6755e01D7970860c445' --> dappwalltest0.aragonid.eth
 // '0x40923e3215243b4a51bf411f9873d02f5bacfd60' --> voting app proxy
@@ -12,22 +11,22 @@ dappwallDAO.init()
 .then( () => {
     // console.log('notifications are ', dappwallDAO.notifications)
     // console.log('aclProxy is ', dappwallDAO.aclProxy)
-    console.log('past events options are ', dappwallDAO.pastEventsOptions)
+    console.log('\n\n past events options are ', dappwallDAO.pastEventsOptions)
 
     // dappwallDAO.pastEvents()
     // .then( data => {
     //     console.log('data from pastEvents', data) // working!
     // })
 
-    dappwallDAO.runApp('0x40923e3215243b4a51bf411f9873d02f5bacfd60')
+    dappwallDAO.getApp('0x40923e3215243b4a51bf411f9873d02f5bacfd60')
     .then( data => {
-        console.log('data is ', data)
+        console.log('\n\n data is ', data)
     })
 
-    // dappwallDAO.getApp('0x40923e3215243b4a51bf411f9873d02f5bacfd60')
-    // .then( data => {
-    //     console.log('data is ', data)
-    // })
-
+    dappwallDAO.runApp('0x40923e3215243b4a51bf411f9873d02f5bacfd60')
+    .then( () => {
+        // console.log(dappwallDAO.handler())
+        // handlers.createRequestHandler(request$, 'past_events', handlers.pastEvents),
+    })
 
 })
