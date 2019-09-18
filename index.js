c1e94c951efd72089135968de46c952a3c3ae1c8
anagutjor <anagutjor@gmail.com>
2019-09-18 19:39:08 +0200

    change dappwallDAO to yourDAO

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Aragon from './src/index';
 
-const dappwallDAO = new Aragon('0xeeda0bD313b8042B360Ac6755e01D7970860c445',
+const yourDAO = new Aragon('0xeeda0bD313b8042B360Ac6755e01D7970860c445',
     {apm: {ensRegistryAddress: '0x98df287b6c145399aaa709692c8d308357bc085d'}},
 ) // rinkeby ensRegistry
 
@@ -8,18 +8,18 @@ const dappwallDAO = new Aragon('0xeeda0bD313b8042B360Ac6755e01D7970860c445',
 // '0x40923e3215243b4a51bf411f9873d02f5bacfd60' --> voting app proxy
 // '0xe2e1ff165a08bcd7a127a77ea50d0cfdad580510' --> acl proxy
 
-dappwallDAO.init()
+yourDAO.init()
 .then( () => {
 
-    console.log('\n\n past events options are ', dappwallDAO.pastEventsOptions)
+    console.log('\n\n past events options are ', yourDAO.pastEventsOptions)
     // first block 4843439
 
-    // dappwallDAO.pastEvents()
+    // yourDAO.pastEvents()
     // .then( data => {
     //     console.log('data from past events', data) // working!
     // })
 
-    dappwallDAO.currentEvents()
+    yourDAO.currentEvents()
     .then( data => {
         console.log('\n\n data from current events', data) // working!
     })
